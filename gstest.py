461c679c323037586f1790468566b1375d204eea
bhitov <Benjamin.Hitov@gmail.com>
2015-01-24 16:43:46 -0500

    updates

--- gstest.py
+++ gstest.py
@@ -11,10 +11,16 @@ wks = gc.open("evaluation_NYT_2014_01 (Responses)").sheet1
 def read_responses(sht):
     row = 2
     out = []
+    fields = ['timestamp', 'quantitative rating', 'qualitative rating', 'keywords / tags', 'optional keywords', 'endorsement', 'username']
 
+    # iterate through rows until cell 1 is empty
     while(len(sht.cell(row,1).value) > 0):
         vals = sht.row_values(row)
-        out.append(vals)
+        row_data = {}
+        for i in range(len(vals)):
+            row_data[fields[i]] = vals[i]
+
+        out.append(row_data)
         row += 1
 
     return out
